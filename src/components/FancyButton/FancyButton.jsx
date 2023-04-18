baf24bd55baf9d3a07ba4dbc816fa3780d0c9fe9
Jack <59969032+jackkieny@users.noreply.github.com>
2023-04-18 02:52:48 -0500

    Slight styling changes to 'FancyButton'

--- src/components/FancyButton/FancyButton.jsx
+++ src/components/FancyButton/FancyButton.jsx
@@ -10,6 +10,7 @@ const FancyButton = () => {
       <p className='counter'>{count}</p>
       <button onClick={() => {setCount(count+1)}} className='fancy-button'>Fancy Buttons</button>
       <button onClick={() => {setCount(0)}} className='reset-button'>RESET</button>
+      <a href='https://youtu.be/6xNcXwC6ikQ' target='_blank' className='fancy-button-inspiration'>INSPIRATION</a>
     </div>
   )
 }
